d0b5f43afe931fc7aa6d2c3392b60b58c5eeb786
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-24 16:02:18 -0700

    WIP: add motor controls

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,38 @@
 #define UPPER_MOTOR_PIN 6
 #define TOP_ENCODER_PIN 4
 #define BOTTOM_ENCODER_PIN 3
+#define FLASHLIGHT 2
 
-// TODO: define flashlight, emergency stop button, and light sensor pins
+// TODO: define emergency stop button and light sensor pins
 
 const byte MOTOR_PINS[] = {LOWER_MOTOR_PIN, UPPER_MOTOR_PIN};
 vexMotor lowerMotor, upperMotor;
 vexMotor motors[] = {lowerMotor, upperMotor};
 
-void setup() {
-    // initiate motors to their respective pins
-    for (uint8_t i = 0; i < (uint8_t) sizeof(motors); i++) {
-        motors[i].attach(MOTOR_PINS[i]);
-    }
+void moveUp() {
+    lowerMotor.write(255);
+    upperMotor.write(-255);
+}
 
-    //
+void moveDown() {
+    lowerMotor.write(255);
+    upperMotor.write(-255);
+}
+
+void setup() {
+//    // initiate motors to their respective pins
+//    for (uint8_t i = 0; i < (uint8_t) sizeof(motors); i++) {
+//        motors[i].attach(MOTOR_PINS[i]);
+//    }
+
+    lowerMotor.attach(LOWER_MOTOR_PIN);
+
+    lowerMotor.write(255);
+
+
+    pinMode(FLASHLIGHT, OUTPUT);
+    digitalWrite(FLASHLIGHT, HIGH);
+
 }
 
 void loop() {
