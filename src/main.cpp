8be4a2702318f79ae37a5af6a18141b0f86fc267
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-26 17:03:58 -0700

    Fix defines

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include <vexMotor.h>
 
-#define LOWER_MOTOR_PIN 5
+#define FLASHLIGHT 7
 #define UPPER_MOTOR_PIN 6
+#define LOWER_MOTOR_PIN 5
 #define TOP_ENCODER_PIN 4
 #define BOTTOM_ENCODER_PIN 3
-#define FLASHLIGHT 2
 
 // TODO: define emergency stop button and light sensor pins
 
