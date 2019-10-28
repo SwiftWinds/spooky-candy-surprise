c9def65cc218b61bfe4035faea6f88c88cd8487d
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-27 18:10:44 -0700

    Doesn't work :( maybe try to complete circuit through the arduino and the battery with only the 2-pin

--- src/main.cpp
+++ src/main.cpp
@@ -9,49 +9,11 @@
 
 // TODO: define emergency stop button and light sensor pins
 
-const byte MOTOR_PINS[] = {LOWER_MOTOR_PIN, UPPER_MOTOR_PIN};
-vexMotor lowerMotor, upperMotor;
-vexMotor motors[] = {lowerMotor, upperMotor};
-
-void moveUp(byte speed = 255) {
-    lowerMotor.write(speed);
-    upperMotor.write(-speed);
-}
-
-void moveDown(byte speed = 255) {
-    lowerMotor.write(-speed);
-    upperMotor.write(speed);
-}
-
-void stop() {
-    for (vexMotor motor : motors) {
-        motor.write(0);
-    }
-}
-
-void initMotors() {
-    upperMotor.attach(UPPER_MOTOR_PIN);
-    lowerMotor.attach(LOWER_MOTOR_PIN);
-}
-
 void setup() {
-//    // initiate motors to their respective pins
-//    for (uint8_t i = 0; i < (uint8_t) sizeof(motors); i++) {
-//        motors[i].attach(MOTOR_PINS[i]);
-//    }
-
-    initMotors();
-
-
     pinMode(FLASHLIGHT, OUTPUT);
     digitalWrite(FLASHLIGHT, HIGH);
 }
 
 void loop() {
-    // wait until light level below threshold
-    // move hand up x degrees of rotation
-    // close hand
-    // wait until button press
-    // open hand
-    // move hand down -x degrees of rotation
+
 }
