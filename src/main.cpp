24fd906882033d7a96e56c29b91e6bc574b3cc62
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-30 15:24:32 -0700

    Add hand servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <vexMotor.h>
 #include <Encoder.h>
+#include <Servo.h>
 
-#define LIGHT_SENSOR_PIN 9
+#define LIGHT_SENSOR_PIN 10
+#define HAND_SERVO_PIN 9
 #define BUTTON_PIN 8
-#define FLASHLIGHT 7
+#define FLASHLIGHT_PIN 7
 #define UPPER_MOTOR_PIN 6
 #define LOWER_MOTOR_PIN 5
 #define TOP_ENCODER_PIN 4
@@ -14,12 +16,16 @@
 
 #define BAUD_RATE 9600
 
+#define BUTTON_DELAY 30
 #define MOTOR_DIST 424
+#define HAND_SERVO_ROTATION 90
 
 const byte MOTOR_PINS[] = {UPPER_MOTOR_PIN, LOWER_MOTOR_PIN};
 vexMotor lowerMotor, upperMotor;
 vexMotor motors[] = {lowerMotor, upperMotor};
 Encoder encoder(BOTTOM_ENCODER_PIN, TOP_ENCODER_PIN);
+
+Servo handServo;
 
 long prevPos, prevY, origPos = -999;
 bool isCallibratingOrigPos = true;
@@ -28,36 +34,37 @@ void moveUp(byte speed = 255) {
     for (vexMotor motor : motors) {
         motor.write(speed);
     }
-    Serial.println(String("moving up at ") + speed + " velocity...");
+    // Serial.println(String("moving up at ") + speed + " velocity...");
 }
 
 void moveDown(byte speed = 255) {
     for (vexMotor motor : motors) {
         motor.write(-speed);
     }
-    Serial.println(String("moving down at ") + speed + " velocity...");
+    // Serial.println(String("moving down at ") + speed + " velocity...");
 }
 
 void stop() {
     for (vexMotor motor : motors) {
         motor.write(0);
     }
-    Serial.println("stopped.");
+    // Serial.println("stopped.");
 }
 
 void initMotors() {
     upperMotor.attach(UPPER_MOTOR_PIN);
     lowerMotor.attach(LOWER_MOTOR_PIN);
-    Serial.println("Initializing motors...");
+    // Serial.println("Initializing motors...");
 }
 
 void setup() {
-    Serial.begin(BAUD_RATE);
-
     initMotors();
 
-    pinMode(FLASHLIGHT, OUTPUT);
-    digitalWrite(FLASHLIGHT, HIGH);
+    handServo.attach(HAND_SERVO_PIN);
+
+    pinMode(BUTTON_PIN, INPUT);
+    pinMode(FLASHLIGHT_PIN, OUTPUT);
+    digitalWrite(FLASHLIGHT_PIN, HIGH);
 }
 
 void loop() {
@@ -74,13 +81,13 @@ void loop() {
         const long currPos = encoder.read();
         if (currPos != prevPos) {
             prevPos = currPos;
-//            Serial.println(String("Encoder: ") + currPos);
+//            // Serial.println(String("Encoder: ") + currPos);
         }
 
         const long currY = analogRead(JOY_Y);
         if (currY != prevY) {
             prevY = currY;
-        Serial.println(String("y: ") + currY);
+            // Serial.println(String("y: ") + currY);
             const long actualY = map(currY, 0, 1023, 255, -255);
             if (actualY > 0) {
                 moveUp(actualY);
@@ -92,17 +99,27 @@ void loop() {
         bool wasInLoop = false;
         boolean lastButton = LOW;
         boolean currentButton = digitalRead(BUTTON_PIN);
+        Serial.println(String("lastButton: ") + lastButton + ", currentButton: " + currentButton);
         while (lastButton != currentButton) {
+            Serial.println(String("In loop; before delay with lastButton: ") + lastButton + ", currentButton: " +
+                           currentButton);
             wasInLoop = true;
-            delay(30);
+            delay(BUTTON_DELAY);
+            Serial.println(String("after delay with lastButton: ") + lastButton + ", currentButton: " + currentButton);
             lastButton = currentButton;
             currentButton = digitalRead(BUTTON_PIN);
         }
         if (wasInLoop) {
+            Serial.println(String("exited loop with lastButton: ") + lastButton + ", currentButton: " + currentButton);
             origPos = currPos;
             isCallibratingOrigPos = false;
 
             Serial.println(String("origPos set to: ") + origPos);
+        } else {
+            Serial.println(
+                    String("failed loop because lastButton: ") + lastButton + ", currentButton: " + currentButton);
         }
+    } else {
+        Serial.println(String("button value: ") + digitalRead(BUTTON_PIN));
     }
 }
