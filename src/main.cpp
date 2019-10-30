38f8a3202fa45b0ed6147eed0044c5503f77bf26
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-30 10:18:08 -0700

    Add encoder calibration
     - To make sure the hand is in the right position at the start and end

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,15 @@
 
 #define BAUD_RATE 9600
 
+#define MOTOR_DIST 424
+
 const byte MOTOR_PINS[] = {UPPER_MOTOR_PIN, LOWER_MOTOR_PIN};
 vexMotor lowerMotor, upperMotor;
 vexMotor motors[] = {lowerMotor, upperMotor};
 Encoder encoder(BOTTOM_ENCODER_PIN, TOP_ENCODER_PIN);
 
-long prevPos, prevX, prevY = -999;
+long prevPos, prevX, prevY, origPos = -999;
+bool isCallibratingOrigPos = true;
 
 void moveUp(byte speed = 255) {
     for (vexMotor motor : motors) {
@@ -67,21 +70,41 @@ void loop() {
     // move hand down -x degrees of rotation
 //    delay(500);
 //    stop();
-    const long currPos = encoder.read();
-    if (currPos != prevPos) {
-        prevPos = currPos;
-        Serial.println(String("Encoder: ") + currPos);
-    }
 
-    const long currY = analogRead(JOY_Y);
-    if (currY != prevY) {
-        prevY = currY;
+    if (isCallibratingOrigPos) {
+        const long currPos = encoder.read();
+        if (currPos != prevPos) {
+            prevPos = currPos;
+//            Serial.println(String("Encoder: ") + currPos);
+        }
+
+        const long currY = analogRead(JOY_Y);
+        if (currY != prevY) {
+            prevY = currY;
         Serial.println(String("y: ") + currY);
-        const long actualY = 128 - map(currY, 0, 1023, 0, 256);
-        if (actualY > 0) {
-            moveUp(actualY);
-        } else {
-            moveDown(actualY);
+            const long actualY = map(currY, 0, 1023, 255, -255);
+            if (actualY > 0) {
+                moveUp(actualY);
+            } else {
+                moveDown(-actualY);
+            }
+        }
+
+        bool wasInLoop = false;
+        boolean lastButton = LOW;
+        boolean currentButton = digitalRead(BUTTON_PIN);
+        while (lastButton != currentButton) {
+            wasInLoop = true;
+            delay(30);
+            lastButton = currentButton;
+            currentButton = digitalRead(BUTTON_PIN);
+        }
+        if (wasInLoop) {
+            origPos = currPos;
+            isCallibratingOrigPos = false;
+
+            Serial.println(String("origPos set to: ") + origPos);
         }
     }
+
 }
