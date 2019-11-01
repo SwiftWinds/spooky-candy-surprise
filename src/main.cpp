79277c70f025c9ad431d80dd1e299bfd2f127795
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-31 17:18:10 -0700

    Add state machine

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@
 #define BUTTON_DELAY 30
 #define MOTOR_DIST 424
 #define HAND_SERVO_ROTATION 90
+#define 
 
-const byte MOTOR_PINS[] = {UPPER_MOTOR_PIN, LOWER_MOTOR_PIN};
 vexMotor lowerMotor, upperMotor;
 vexMotor motors[] = {lowerMotor, upperMotor};
 Encoder encoder(BOTTOM_ENCODER_PIN, TOP_ENCODER_PIN);
@@ -28,7 +28,27 @@ Encoder encoder(BOTTOM_ENCODER_PIN, TOP_ENCODER_PIN);
 Servo handServo;
 
 long prevPos, prevY, origPos = -999;
-bool isCallibratingOrigPos = true;
+enum state {
+    calibratingOrigPos,
+    waitLightThreshold,
+    movingHandUp,
+    closingHand,
+    waitOpenHandButtonPress,
+    openingHand,
+    movingHandDown
+};
+
+state _state = calibratingOrigPos;
+
+state getState() {
+    return _state;
+}
+
+bool setState(state newState) {
+    auto temp = _state;
+    _state = newState;
+    return temp == newState;
+}
 
 void moveUp(byte speed = 255) {
     for (vexMotor motor : motors) {
@@ -58,6 +78,8 @@ void initMotors() {
 }
 
 void setup() {
+    Serial.begin(BAUD_RATE);
+
     initMotors();
 
     handServo.attach(HAND_SERVO_PIN);
@@ -68,6 +90,71 @@ void setup() {
 }
 
 void loop() {
+    switch (getState()) {
+        case calibratingOrigPos: {
+            const long currPos = encoder.read();
+            if (currPos != prevPos) {
+                prevPos = currPos;
+                // Serial.println(String("Encoder: ") + currPos);
+            }
+
+            const long currY = analogRead(JOY_Y);
+            if (currY != prevY) {
+                prevY = currY;
+                // Serial.println(String("y: ") + currY);
+                const long actualY = map(currY, 0, 1023, 255, -255);
+                if (actualY > 0) {
+                    moveUp(actualY);
+                } else {
+                    moveDown(-actualY);
+                }
+            }
+
+            bool wasInLoop = false;
+            boolean lastButton = LOW;
+            boolean currentButton = digitalRead(BUTTON_PIN);
+            Serial.println(String("lastButton: ") + lastButton + ", currentButton: " + currentButton);
+            while (lastButton != currentButton) {
+                Serial.println(String("In loop; before delay with lastButton: ") + lastButton + ", currentButton: " +
+                               currentButton);
+                wasInLoop = true;
+                delay(BUTTON_DELAY);
+                Serial.println(
+                        String("after delay with lastButton: ") + lastButton + ", currentButton: " + currentButton);
+                lastButton = currentButton;
+                currentButton = digitalRead(BUTTON_PIN);
+            }
+            if (wasInLoop) {
+                Serial.println(
+                        String("exited loop with lastButton: ") + lastButton + ", currentButton: " + currentButton);
+                origPos = currPos;
+                setState(waitLightThreshold);
+
+                Serial.println(String("origPos set to: ") + origPos);
+            } else {
+                Serial.println(
+                        String("failed loop because lastButton: ") + lastButton + ", currentButton: " + currentButton);
+            }
+            break;
+        }
+        case waitLightThreshold:
+            Serial.println(String("button value: ") + digitalRead(BUTTON_PIN));
+            if ()
+            break;
+        case movingHandUp:
+            if (encoder.read() - origPos >= HAND_SERVO_ROTATION) {
+
+            }
+            break;
+        case closingHand:
+            break;
+        case waitOpenHandButtonPress:
+            break;
+        case openingHand:
+            break;
+        case movingHandDown:
+            break;
+    }
     // wait until light level below threshold
     // move hand up x degrees of rotation
     // close hand
@@ -76,50 +163,4 @@ void loop() {
     // move hand down -x degrees of rotation
 //    delay(500);
 //    stop();
-
-    if (isCallibratingOrigPos) {
-        const long currPos = encoder.read();
-        if (currPos != prevPos) {
-            prevPos = currPos;
-//            // Serial.println(String("Encoder: ") + currPos);
-        }
-
-        const long currY = analogRead(JOY_Y);
-        if (currY != prevY) {
-            prevY = currY;
-            // Serial.println(String("y: ") + currY);
-            const long actualY = map(currY, 0, 1023, 255, -255);
-            if (actualY > 0) {
-                moveUp(actualY);
-            } else {
-                moveDown(-actualY);
-            }
-        }
-
-        bool wasInLoop = false;
-        boolean lastButton = LOW;
-        boolean currentButton = digitalRead(BUTTON_PIN);
-        Serial.println(String("lastButton: ") + lastButton + ", currentButton: " + currentButton);
-        while (lastButton != currentButton) {
-            Serial.println(String("In loop; before delay with lastButton: ") + lastButton + ", currentButton: " +
-                           currentButton);
-            wasInLoop = true;
-            delay(BUTTON_DELAY);
-            Serial.println(String("after delay with lastButton: ") + lastButton + ", currentButton: " + currentButton);
-            lastButton = currentButton;
-            currentButton = digitalRead(BUTTON_PIN);
-        }
-        if (wasInLoop) {
-            Serial.println(String("exited loop with lastButton: ") + lastButton + ", currentButton: " + currentButton);
-            origPos = currPos;
-            isCallibratingOrigPos = false;
-
-            Serial.println(String("origPos set to: ") + origPos);
-        } else {
-            Serial.println(
-                    String("failed loop because lastButton: ") + lastButton + ", currentButton: " + currentButton);
-        }
-    } else {
-        Serial.println(String("button value: ") + digitalRead(BUTTON_PIN));
-    }
 }
